1661b1882d81e581be76449e98270b5ced4b7e13
bryanzhou <bzhou07@students.cs.ubc.ca>
2023-02-21 02:18:00 -0800

    Further implemented navigation system

--- src/main/model/PokemonCard.java
+++ src/main/model/PokemonCard.java
@@ -9,11 +9,12 @@ public class PokemonCard extends Card{
     private String pokeType;
     private Boolean holofoil;
     private int hitPoints;
+    private int Stage;
     private List<Move> moves;
 
-    //REQUIRES: Hitpoints > 0
+    //REQUIRES: Hitpoints > 0, Stage in [1,2,3]
     //EFFECTS: Constructor for PokemonCard class
-    public PokemonCard(String pokeName, String pokeType, Boolean holofoil, int hitPoints, List<Move> moves) {
+    public PokemonCard(String pokeName, String pokeType, Boolean holofoil, int hitPoints, int Stage, List<Move> moves) {
         this.pokeName = pokeName;
         this.pokeType = pokeType;
         this.holofoil = holofoil;
@@ -34,14 +35,19 @@ public class PokemonCard extends Card{
         return pokeType;
     }
 
+    //EFFECTS: Getter for the holofoil status field
+    public Boolean getHolofoil() {
+        return holofoil;
+    }
+
     //EFFECTS: Getter for the Pokemon cards hitpoints field
     public int getHitPoints() {
         return hitPoints;
     }
 
-    //EFFECTS: Getter for the holofoil status field
-    public Boolean getHolofoil() {
-        return holofoil;
+    //EFFECTS: Getter for the Pokemon cards stage field
+    public int getStage() {
+        return Stage;
     }
 
     //EFFECTS: Getter for the moves field
--- src/main/ui/CollectionApp.java
+++ src/main/ui/CollectionApp.java
@@ -22,6 +22,19 @@ public class CollectionApp {
 
         init();
 
+        while (!stop) {
+            mainMenu();
+
+            command = input.next().toLowerCase();
+            if (command == "q") {
+                stop = true;
+            } else {
+                redirect(command);
+            }
+
+        }
+
+        System.out.println("Thanks for using My PokeLibrary, see you next time!");
     }
 
     //MODIFIES: this
@@ -36,5 +49,74 @@ public class CollectionApp {
         userName = input.next();
         System.out.println("\nWelcome " + userName + "!");
     }
+
+    //EFFECTS: Prints out the directory for the various options user could take
+    public void mainMenu() {
+        System.out.println(userName + ", input one of the options below: \n" +
+                "A - Add card to collection \n" +
+                "V - View the cards in your collection\n" +
+                "D - Create a new deck\n" +
+                "Q - Quit application\n");
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes users command
+    public void redirect(String command) {
+        if (command.equals("A")) {
+            System.out.println("Done A"); //addCards
+        } else if (command.equals("V")) {
+            System.out.println("Done V"); //view
+        } else if (command.equals("D")) {
+            System.out.println("Done D"); //deck (hard)
+        } else {
+            System.out.println("Input not recognized, please type in a recognized input\n");
+        }
+    }
+
+    //MODIFIES: cardsList
+    //EFFECTS: Adds one or multiple of the same card to your collection
+    public void addCards() {
+        int toAddCount;
+        String command;
+
+        System.out.println("What type of pokemon card are you trying to add?\n" + "P - Pokemon cards\n" +
+                "T - Trainer cards\n" + "E - Energy cards\n" + "R - Return to main menu\n");
+        command = input.next().toLowerCase();
+
+        System.out.println("\nHow many copies of that card do you want to add?");
+        toAddCount = input.nextInt();
+
+        if (!command.equals("r") && "pte".contains(command)) {
+            Card toAdd = toAddCreator(command);
+
+            for (int i = 0; i < toAddCount; i++) {
+                cardsList.addCard(toAdd);
+            }
+
+            System.out.println("\n" + String.valueOf(toAddCount) + " cards successfully added to collection!\n");
+        } else if (!command.equals("r")) {
+            System.out.println("\nInput not recognized, redirecting back to main menu...\n");
+        } else {
+            System.out.println("\nRedirecting back to main menu...\n");
+        }
+    }
+
+    //EFFECTS: constructs and returns the proper card type
+    public Card toAddCreator(String command) {
+        /*if (command.equals()) {
+
+        } else if () {
+
+        } else {
+
+        }*/
+        return new EnergyCard("placeholder", false);
+    }
+
+    //addPkmn, addTrainer, addEnergy
+
+    //EFFECTS: Initializes a new pokemonCard object according to inputs
+    //EFFECTS: Initializes a new trainerCard object according to inputs
+    //EFFECTS: Initializes a new energyCard object according to inputs
 
 }
